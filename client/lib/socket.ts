8720f97c2bb8834e18dbc471a44d9852f8a367e5
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-19 15:49:17 +0500

    NEXT_PUBLIC_WEB_SOCKET_DOMAIN

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,9 +2,11 @@
 import { io, Socket } from "socket.io-client";
 
 const socket: Socket = io(
-  process.env.WEB_SOCKET_DOMAIN || "ws://memory-game-socket.onrender.com",
+  process.env.NEXT_PUBLIC_WEB_SOCKET_DOMAIN ||
+    "ws://memory-game-socket.onrender.com",
   {
     autoConnect: false,
+    transports: ["websocket"], // Polling o‘rniga WebSocket to‘g‘ridan-to‘g‘ri
   }
 );
 
